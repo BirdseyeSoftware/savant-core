51bbeffcf04acd64dc9d21d3ce37b4ec99cb437d
Roman Gonzalez <romanandreg@gmail.com>
2012-11-08 22:43:23 +0000

    Removing lein-swank from dependencies
    Also moving builds ids inside build map and using a vector instead of
    a map to have ordered builds

--- project.clj
+++ project.clj
@@ -11,39 +11,48 @@
                  [org.clojure/google-closure-library "0.0-1376-2"]
                  [buster-cljs "0.1.0-SNAPSHOT"]]
 
-  :plugins [[lein-swank "1.4.4"]
-            [lein-cljsbuild "0.2.9"]
+  :plugins [[lein-cljsbuild "0.2.9"]
             [lein-dalap-cljsbuild "0.1.0-SNAPSHOT"]]
-
   :hooks [leiningen.dalap-cljsbuild]
+
   :source-paths ["src/clj" "src/cljs"]
   :test-paths ["test/clj" "test/cljs"]
 
   :cljsbuild
   {:builds
-   [;; :dev
-    {:source-path "src/cljs"
+   [{:id :dev
+     :source-path "src/cljs"
      :dalap
      {:paths
-      {"src/clj/savant/util.clj" "src/cljs/savant/util.cljs"}}
+      {"src/clj/savant/core.clj" "src/cljs/savant/core.cljs"
+       "src/clj/savant/util.clj" "src/cljs/savant/util.cljs"
+       "src/clj/savant/store/memory.clj" "src/cljs/savant/store/memory.cljs"}}
      :compiler
      {:output-to "resources/js/savant_core_dev.js"
       :optmizations :whitespace
       :pretty-print true}}
-    ;;; :browser-test
-    {:source-path "test/cljs"
+    ;;;
+    {:id :browser-test
+     :source-path "test/cljs"
      :dalap
      {:paths
       {"test/clj/savant/test/util_test.clj"
-       "test/cljs/savant/test/util_test.cljs"}}
+       "test/cljs/savant/test/util_test.cljs"
+
+       "test/clj/savant/test/store_spec.clj"
+       "test/cljs/savant/test/store_spec.cljs"
+
+       "test/clj/savant/test/store/memory_test.clj"
+       "test/cljs/savant/test/store/memory_test.cljs"}}
      :compiler
      {:externs ["externs/buster.js"]
       :libraries ["resources/js/savant_core_dev.js"]
       :output-to "resources/js/savant_core_browser_test.js"
       :optimizations :simple
       :pretty-print true}}
-    ;;; :node-test
-    {:source-path "test/cljs"
+    ;;;
+    {:id :node-test
+     :source-path "test/cljs"
      :compiler
      {:externs ["externs/buster.js"]
       :libraries ["resources/js/savant_core_dev.js"]
