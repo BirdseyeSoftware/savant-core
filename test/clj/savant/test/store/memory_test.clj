51c880a263781069b85c6455d688ec8e87f8b1ee
Roman Gonzalez <romanandreg@gmail.com>
2012-11-08 22:47:42 +0000

    Making tests buster compatible by using buster-cljs.clojure API

--- test/clj/savant/test/store/memory_test.clj
+++ test/clj/savant/test/store/memory_test.clj
@@ -1,11 +1,26 @@
+^{:cljs
+  '(ns savant.test.store.memory-test
+     (:require [buster-cljs.core :refer [is]]
+               [savant.test.store-spec
+                :refer
+                [get-event-store-constructor-tests
+                 event-store-protocol-tests
+                 event-stream-protocol-tests]]
+               [savant.store.memory :as m])
+     (:require-macros [buster-cljs.macros
+                       :refer [initialize-buster deftest describe]]))}
 (ns savant.test.store.memory-test
   (:require [clojure.test :refer :all]
+            [buster-cljs.clojure :refer [describe]]
             [savant.test.store-spec :refer :all]
             [savant.store.memory]))
 
+#_(:cljs
+   (initialize-buster))
+
 (deftest memory-event-store-tests
-  (testing "testing memory event store:"
-    (let [ns 'savant.store.memory
+  (describe "testing memory event store:"
+    (let [ns ^{:cljs '(js* "savant.store.memory")} 'savant.store.memory
           valid-opts {:name "foo"}
           invalid-opts {:invalid "option"}]
       (get-event-store-constructor-tests ns valid-opts invalid-opts)
--- test/clj/savant/test/util_test.clj
+++ test/clj/savant/test/util_test.clj
@@ -3,17 +3,18 @@
     (:require [buster-cljs.core :refer [is]]
               [savant.util :as u])
     (:require-macros [buster-cljs.macros
-                      :refer [initialize-buster deftest testing]]))}
+                      :refer [initialize-buster deftest it]]))}
 (ns savant.test.util-test
-  (:require [clojure.test :refer [deftest testing is]])
-  (:require [savant.util :as u]))
+  (:require [clojure.test :refer [deftest is]]
+            [buster-cljs.clojure :refer [it]]
+            [savant.util :as u]))
 
 #_(:cljs
 (initialize-buster))
 
 (deftest test-hex-digest
   ;; testing both for clj && cljs
-  (testing "digest of number 1 has always the same value"
+  (it "digest of number 1 has always the same value"
     (is (= "356a192b7913b04c54574d18c28d46e6395428ab"
            (u/hex-digest 1))
         "hex-digest should return always same value for number 1")))
